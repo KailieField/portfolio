fe7233fb452b435fa44925e5fb1c9dd7b098b46e
Kailie Field <kailie.field@gmail.com>
2025-03-17 16:54:07 -0400

    updated workexperienceview to include image, reformatted

--- portfolio/portfolio/Views/WorkExperienceView.swift
+++ portfolio/portfolio/Views/WorkExperienceView.swift
@@ -13,18 +13,29 @@ struct WorkExperienceView: View {
                 VStack(spacing: 20){
                     ForEach(experience, id: \.jobTitle){ job in
                         VStack(alignment: .leading){
+                            
+                            Image("medical_badge")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 230, height: 230)
+                                .padding(.bottom, 20)
+                                .frame(maxWidth: .infinity)
+                            
                             Text(job.jobTitle)
                                 .font(.system(size: 20, weight: .bold, design: .monospaced))
                                 .padding(.bottom, 2)
                                 .foregroundColor(.white)
+                            
                             Text(job.company)
                                 .font(.system(size: 15, weight: .ultraLight, design: .monospaced))
                                 .foregroundColor(.gray)
                                 .padding(.bottom, 2)
+                            
                             Text("From \(job.startDate) to \(job.endDate)")
                                 .font(.system(size: 15, weight: .bold, design: .monospaced))
                                 .padding(.bottom, 2)
                                 .foregroundColor(.white)
+                            
                             Text(job.description)
                                 .font(.system(size: 13, weight: .ultraLight, design: .monospaced))
                                 .padding(.bottom, 2)
